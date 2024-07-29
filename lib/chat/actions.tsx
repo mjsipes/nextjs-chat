a6b742e8a24f9c59f0c45241983faa98eeb2f725
mjsipes <msipes@usc.edu>
2024-07-29 14:54:05 -0700

    fixc

--- lib/chat/actions.tsx
+++ lib/chat/actions.tsx
@@ -14,6 +14,7 @@ import { z } from 'zod'
 import { nanoid } from '@/lib/utils'
 import { saveChat } from '@/app/actions'
 import { auth } from '@/auth'
+import { Chat, Message } from '@/lib/types'
 
 async function submitUserMessage(content: string) {
   'use server'
